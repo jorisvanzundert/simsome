49573501b6f849c8401a13ef5518058aa03183d9
Joris van Zundert <joris.van.zundert@gmail.com>
2018-10-12 01:57:19 +0200

    Added functionality to animate simulation.

--- landscape.rb
+++ landscape.rb
@@ -1,4 +1,5 @@
 require_relative 'plasma_fractal'
+require 'open3'
 
 class Landscape
 
@@ -43,14 +44,15 @@ class Landscape
     map
   end
 
-  def gnuplot(commands)
-    IO.popen("gnuplot", "w") { |io| io.puts commands }
+  def gnuplot( commands )
+    IO.popen( "gnuplot", "w" ) { |io| io.puts commands }
   end
 
-  def serialize( agent_path_data: nil )
-    if agent_path_data != nil
-      File.open( "tmp/agent_path.dat", 'w') { |file| file.write( agent_path_data ) }
-    end
+  def apngasm( commands )
+    IO.popen( "apngasm", "w" ) { |io| io.puts commands }
+  end
+
+  def serialize_grid
     File.open( "tmp/#{@map_name}.dat", 'w') { |file| file.write( self.xyz_map ) }
     commands = %Q(
       set table "tmp/#{@map_name}_grid_data.dat"
@@ -58,21 +60,68 @@ class Landscape
       splot "tmp/#{@map_name}.dat" u 1:2:3
       unset table
       unset dgrid3d
+    )
+    gnuplot( commands )
+  end
+
+  def serialize_plot( set_commands: nil, splot_commands: nil, frame_number: nil )
+    frame_suffix = "_%04d" % frame_number if frame_number != nil
+    commands = %Q(
       set terminal png size 4096,3072
-      set output "tmp/#{@map_name}.png"
+      set output "tmp/#{@map_name}#{frame_suffix}.png"
       set zrange[150:650]
       set view 60, 130
       set xlabel "x"
       set ylabel "y"
       set style data lines
       set hidden3d
-      splot 'tmp/#{@map_name}_grid_data.dat' u 1:2:3 w lines,\
-            'tmp/agent_path.dat' u 1:2:3 w lines lc rgb '#ff2222',\
-            'tmp/agent_path_start.dat' u 1:2:3 w points pointtype 7 lc rgb '#22ff22',\
-            'tmp/agent_path_end.dat' u 1:2:3 w points pointtype 7 lc rgb '#2222ff'
+      #{set_commands}
+      splot 'tmp/#{@map_name}_grid_data.dat' u 1:2:3 w lines#{splot_commands}
       # w pm3d -> isolines heat map
     )
-    gnuplot(commands)
+    gnuplot( commands )
+  end
+
+  def serialize( agent_path_data: nil, animate: false )
+    serialize_grid
+    start_marker = nil
+    end_marker = nil
+    if agent_path_data != nil
+      File.open( "tmp/agent_path.dat", 'w') { |file| file.write( agent_path_data.join( "\n" ) ) }
+      start_marker_xyz = agent_path_data[0].split( " " ).join( "," )
+      end_marker_xyz = agent_path_data[-1].split( " " ).join( "," )
+      set_start_marker = "      set object circle at #{start_marker_xyz} size 0.5 fc rgb '#2222ff' fs solid 1.0"
+      set_end_marker = "      set object circle at #{end_marker_xyz} size 0.5 fc rgb '#22ff22' fs solid 1.0"
+    end
+    last_frame_index = nil
+    if animate && agent_path_data != nil
+      last_frame_index = agent_path_data.length - 1
+      serialize_first_frame( set_start_marker: set_start_marker )
+      serialize_path_frames( last_frame_index: last_frame_index, set_start_marker: set_start_marker )
+    end
+    serialize_last_frame( last_frame_index: last_frame_index, set_start_marker: set_start_marker, set_end_marker: set_end_marker )
+    # if animate && agent_path_data != nil
+    #   os_map_name = @map_name.gsub( ' ', '\ ' )
+    #   %x( apngasm tmp/#{os_map_name}_ani.png tmp/#{os_map_name}_*.png )
+    # end
+  end
+
+  def serialize_first_frame( set_start_marker: nil )
+    serialize_plot( set_commands: set_start_marker, frame_number: 1 )
+  end
+
+  def serialize_path_frames( last_frame_index: 0, set_start_marker: nil )
+    (0..last_frame_index).each do |index|
+      splot = ",\\\n            'tmp/agent_path.dat' u 1:2:3 every ::0::#{index} w lines lc rgb '#ff2222'"
+      serialize_plot( set_commands: set_start_marker, splot_commands: splot,  frame_number: (index + 2) )
+    end
+  end
+
+  def serialize_last_frame( last_frame_index: nil, set_start_marker: nil, set_end_marker: nil )
+    set_commands = "#{ set_start_marker }\n#{ set_end_marker }"
+    splot = ",\\\n            'tmp/agent_path.dat' u 1:2:3 w lines lc rgb '#ff2222'"
+    last_frame_index += 3 if last_frame_index != nil
+    serialize_plot( set_commands: set_commands, splot_commands: splot,  frame_number: last_frame_index )
   end
 
 end
